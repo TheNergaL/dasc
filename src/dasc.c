e37e673fb06818ed16ab3ab35ada41d3945f1203
tuckerpo <tuckerpo@buffalo.edu>
2017-10-20 22:50:54 -0400

    void method for passing in arg3 for pthread

--- src/dasc.c
+++ src/dasc.c
@@ -89,19 +89,676 @@ bool checkClearAirspace(int x, int y) {
    return false;
 }
 
+void avoidCollision(int i, int j, int x, int y){
+	char s;
+	s='+';
+	//for the case where the home position vertical coordinate is less than destination position vertical coordinate
+	if(j<y){
+			//check if the block of space upwards is open or not
+			if(checkClearAirspace(i-1,j)==true){
+					//move vertical upwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+					
+					//move horizontal right one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;	
+
+					//move horizontal right one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;	
+
+					//move vertical downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;	
+					
+			}else{
+					//if block of space upwards is not open, move vertically downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;	
+					
+					//move horizontal right one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;	
+
+					//move horizontal right one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;	
+
+					//move vertical upwards one time 
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+				
+			}	
+		//successfully avoided collision!
+		return;				
+	}
+	//for the case where the home position vertical coordinate is more than destination position vertical coordinate
+	if(j>y){
+			//check if the block of space upwards is open or not
+			if(checkClearAirspace(i-1,j)==true){
+					//move vertically upwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;	
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+
+					//move horizonal left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;	
+
+					//move horizontal left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;	
+
+					//move vertically downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;	
+	
+			}else{
+					//if block of space upwards is not open move vertically downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;	
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;	
+
+					//move horizontal left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;	
+
+					//move horizontal left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;	
+
+					//move vertically upwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+						
+			}
+		//collision succesfully avoided!
+		return;
+	}
+	//for the case where the home position horizontal coordinate is less than destination position horizontal coordinate 
+	if(i<x){
+			//check if the block of space on the left is open or not
+			if(checkClearAirspace(i,j-1)==true){
+					//move horizontal left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;	
+
+					//move vertical downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;	
+
+					//move vertical downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;
+
+					//move horizontal right one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;
+
+			}else{
+					//if the block of space on left is not open move right
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;	
+
+					//move vertical downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;	
+
+					//move vertical downwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i+1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i++;
+
+					//move horizontal left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;
+				
+			}
+		//succesfully avoided collision!
+		return;
+	
+	}
+	//for the case where the home position horizontal coordinate is more than destination position horizontal coordinate 
+	if(i>x){
+				//check if the block of space on the right is open or not
+				if(checkClearAirspace(i,j+1)==true){
+					//move horizontal right one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;	
+
+					//move vertical upwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+
+					//move vertical upwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+
+					//move horizontal left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;	
+
+				}else{
+					//if the block of space on right is not open move left one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j-1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j--;	
+
+					//move vertical upwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+
+					//move vertically upwards one time
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i-1][j]=' ';
+					pthread_mutex_unlock(&mtx);
+					i--;	
+
+					//move horizontal right one time
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=s;
+					pthread_mutex_unlock(&mtx);
+					system("clear");
+					outputAirspace();
+					sleep(1);
+					pthread_mutex_lock(&mtx);
+					airspace[i][j+1]=' ';
+					pthread_mutex_unlock(&mtx);
+					j++;	
+
+			}
+		//successfully avoided collision!
+		return;
+	}
+
+}
+
+void moveDrone(int i, int j, int x, int y){
+	char s;
+	s='+';
+	//while the vertical coordinates of drone and destination are not equal
+	while(j!=y){
+		//for the case where the drone position vertical coordinate is less than destination postion vertical coordinate 
+		if((j<y)){
+			//check if the block of space on the right is open or not
+			if(checkClearAirspace(i,j+1)==true){	
+				//move right one time
+				pthread_mutex_lock(&mtx);
+				airspace[i][j+1]=s; 
+				pthread_mutex_unlock(&mtx);
+				//sleep(1);
+				system("clear");
+				outputAirspace();
+				sleep(1);
+				pthread_mutex_lock(&mtx);
+				airspace[i][j+1]=' ';
+				pthread_mutex_unlock(&mtx);
+				j++;
+
+			}else{
+				//if space on the right is occupied avoid collision or signal collision taking place 
+				avoidCollision(i,j,x,y);
+			//	signalD(i,j+1);
+				j=j+2;
+			}
+					
+		}
+		//for the case where the drone position vertical coordinate is more than destination position vertical coordinate
+		if((j>y)){
+			//check if the block of space on the left is open or not
+			if(checkClearAirspace(i,j-1)==true){
+				//move left one time
+				pthread_mutex_lock(&mtx);
+				airspace[i][j-1]=s;
+				pthread_mutex_unlock(&mtx);
+				//sleep(1);
+				system("clear");
+				outputAirspace();
+				sleep(1);
+				pthread_mutex_lock(&mtx);
+				airspace[i][j-1]=' ';
+				pthread_mutex_unlock(&mtx);
+				j--;
+
+			}else{
+				//if the space on the left is occupied avoid collision or signal collision taking place
+				avoidCollision(i,j,x,y);
+			//	signalD(i,j-1);
+				j=j-2;
+			}
+		}
+	}
+	//while the horizontal coordinates of the drone and destination are not equal
+	while(i!=x){
+		//for the case where the drone position horizontal coordinate is less than destination position horizontal coordinate
+		if((i<x)){
+			//check if the block of space downwards is open or not
+			if(checkClearAirspace(i+1,j)==true){
+				//move downwards one time
+				pthread_mutex_lock(&mtx);
+				airspace[i+1][j]=s;	
+				pthread_mutex_unlock(&mtx);
+				//sleep(1);
+				system("clear");
+				outputAirspace();
+				sleep(1);
+				pthread_mutex_lock(&mtx);
+				airspace[i+1][j]=' ';
+				pthread_mutex_unlock(&mtx);
+				i++;
+
+			}else{
+				//if the space downwards is occupied avoid collision or signal collision taking place
+				avoidCollision(i,j,x,y);
+			//	signalD(i+1,j);
+				i=i+2;
+			}
+		}	
+		//for the case where the drone position horizontal coordinate is more than destination position horizontal coordinate
+		if((i>x)){
+			//check if the block of space upwards is open or not
+			if(checkClearAirspace(i-1,j)==true){
+				//move upwards one time
+				pthread_mutex_lock(&mtx);
+				airspace[i-1][j]=s;
+				pthread_mutex_unlock(&mtx);
+				//sleep(1);
+				system("clear");
+				outputAirspace();
+				sleep(1);
+				pthread_mutex_lock(&mtx);
+				airspace[i-1][j]=' ';
+				pthread_mutex_unlock(&mtx);
+				i--;
+
+			}else{
+				//if the space upwards is occupied avoid collision or signal collision taking place
+				avoidCollision(i,j,x,y);
+			//	signalD(i-1,j);
+				i=i-2;
+			}
+		}
+	}
+
+}
+
+void fillMap(int a, int b, int x, int y){
+	
+	pthread_mutex_lock(&mtx);
+	int i,j;
+	for(i=1;i<=50;i++){
+		for(j=1;j<=50;j++){
+		
+			if(i!=50 && i!=1 && j !=50 && j!=1){
+				airspace[i][j]=' ';
+			}else{
+				airspace[i][j]='x';
+			}
+		
+		}
+	
+	}
+	
+	airspace[14][9]='!';
+	airspace[28][39]='!';
+	pthread_mutex_unlock(&mtx);
+	//the moveDrone or navigation method is called here 2 times for the drone to first move from home to destination and then back to home
+	moveDrone(a,b,x,y);
+	moveDrone(x,y,a,b);
+}
+
+void *printDroneX(void *threadid){
+	d0.hx=25;
+	d0.hy=25;
+	d0.dx=9;
+	d0.dy=9;
+	d1.hx=25;
+	d1.hy=25;
+	d1.dx=9;
+	d1.dy=9;
+	d2.hx=25;
+	d2.hy=25;
+	d2.dx=39;
+	d2.dy=39;
+	d3.hx=25;
+	d3.hy=25;
+	d3.dx=39;
+	d3.dy=39;
+	d4.hx=25;
+	d4.hy=25;
+	d4.dx=9;
+	d4.dy=39;
+	d5.hx=25;
+	d5.hy=25;
+	d5.dx=9;
+	d5.dy=39;
+	d6.hx=25;
+	d6.hy=25;
+	d6.dx=39;
+	d6.dy=39;
+	d7.hx=25;
+	d7.hy=25;
+	d7.dx=39;
+	d7.dy=9;
+	d8.hx=25;
+	d8.hy=25;
+	d8.dx=39;
+	d8.dy=9;
+	d9.hx=25;
+	d9.hy=25;
+	d9.dx=39;
+	d9.dy=9;
+
+
+	long tid;
+	tid = (long)threadid;
+	if(tid==0){
+		fillMap(d0.hx, d0.hy, d0.dx, d0.dy);
+	}
+	if(tid==1){
+		fillMap(d1.hx, d1.hy, d1.dx, d1.dy);
+	}
+	if(tid==2){
+		fillMap(d2.hx, d2.hy, d2.dx, d2.dy);
+	}
+	if(tid==3){
+		fillMap(d3.hx, d3.hy, d3.dx, d3.dy);
+	}
+	if(tid==4){
+		fillMap(d4.hx, d4.hy, d4.dx, d4.dy);
+	}
+	if(tid==5){
+		fillMap(d5.hx, d5.hy, d5.dx, d5.dy);
+	}
+	if(tid==6){
+		fillMap(d6.hx, d6.hy, d6.dx, d6.dy);
+	}
+	if(tid==7){
+		fillMap(d7.hx, d7.hy, d7.dx, d7.dy);
+	}
+	if(tid==8){
+		fillMap(d8.hx, d8.hy, d8.dx, d8.dy);
+	}
+	if(tid==9){
+		fillMap(d9.hx, d9.hy, d9.dx, d9.dy);
+	}
+	pthread_exit(NULL);
+}
+
 int main() {
-    pthread_t threads[NUM_DRONES];
-    int i, j;
+    pthread_t threads[NUM_THREADS];
+	int rc;
+	long t=0;
+	long t1=1;
+	long t2=2;
+	long t3=3;
+	long t4=4;
+	long t5=5;
+	long t6=6;
+	long t7=7;
+	long t8=8;
+	long t9=9;
 
-    void* status;
-    pthread_attr_t attr;
+	void *status;
+	pthread_attr_t attr;
 
-    pthread_mutex_init(&mtx, NULL);
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+	pthread_mutex_init(&mtx,NULL);
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+	int i;
+	for(t=0; t<NUM_THREADS;t++){
+		pthread_create(&threads[t],NULL,printDroneX,(void *)t);
+		sleep(2);
+	}
+		pthread_attr_destroy(&attr);
+//		int i;
+		for(i=0;i<NUM_THREADS;i++){
+			pthread_join(threads[i],&status);
+		}
+	pthread_mutex_destroy(&mtx);
+	pthread_exit(NULL);
 
-    for (i = 0; i < NUM_DRONES; i++) {
-        pthread_create(&threads[i],NULL,outputAirspace,(void *)i);
-    }
-
-    }
+}
