5312d2a3efae9a00899549b0b9cbc81d1a748011
tuckerpo <tuckerpo@buffalo.edu>
2017-10-21 18:55:19 -0400

    no movement trail

--- src/dcas.c
+++ src/dcas.c
@@ -149,7 +149,7 @@ void *fly(void *arg0){
         avoid(&curr_x,&curr_y, nextMove, taskID);
       }
       airspace[curr_x][curr_y+1] = taskID;
-      airspace[curr_x][curr_y] = nextMove;
+      airspace[curr_x][curr_y] = '.';
       printMap();
       
       usleep(sleep_2);
@@ -165,7 +165,7 @@ void *fly(void *arg0){
         avoid(&curr_x,&curr_y, nextMove, taskID);
       }
       airspace[curr_x+1][curr_y] = taskID;
-      airspace[curr_x][curr_y] = nextMove;
+      airspace[curr_x][curr_y] = '.';
       printMap();
       usleep(sleep_2);
       
@@ -180,7 +180,7 @@ void *fly(void *arg0){
         avoid(&curr_x,&curr_y, nextMove, taskID);
       }
       airspace[curr_x-1][curr_y] = taskID;
-      airspace[curr_x][curr_y] = nextMove;
+      airspace[curr_x][curr_y] = '.';
       printMap();
       usleep(sleep_2);
       
