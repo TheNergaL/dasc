7543f4359fa446a29112213dd758a90ad44802d0
tuckerpo <tuckerpo@buffalo.edu>
2017-10-21 20:36:41 -0400

    comment code

--- src/dcas.c
+++ src/dcas.c
@@ -4,32 +4,47 @@
 #include <pthread.h>
 #include <time.h>
 
+//preproccesor macros for defining vars
 #define NUM_DRONES 10 
 #define NUM_OBSTACLES 10 
 #define MAP_SIZE 50
 
+//sleep for 2 seconds
 int sleep_2 = 200000;
+
+//global mutex thread for locking and unlocking during collision avoidance
 pthread_mutex_t mtx;
 
+//2d char array of size 50*50 which holds drones, packages, airports and obstacles
 char airspace[MAP_SIZE][MAP_SIZE];
-
+//create the map
 void makeMap();
+//output map 
 void printMap();
+//fly function for passing into pthreads
 void *fly(void *arg0);
+//place obstacles arbitrarily on the map
 void placeObstacles();
+// check for obstacles (and other drones) for every movement iteration
+// search for a different path to take
 void avoid(int*, int*, char, int);
 
+// contains thread number, airport location and package location for the drones
 struct threadData{
   int thread_id;
   int airport_x,airport_y;
   int package_x,package_y;
 };
+// array of drones
 struct threadData threadDataArr[NUM_DRONES];
 
+// obstacle struct which defines x,y coords of obstacles on the map
 struct obstacles{
     int obs_x;
     int obs_y;
 };
+
+// array of obstacles
 struct obstacles obsArr[NUM_OBSTACLES];
 
 int main (int argc, char **argv)
@@ -38,33 +53,41 @@ int main (int argc, char **argv)
   placeObstacles();
   
   pthread_mutex_init(&mtx, NULL);
-  pthread_t threads[NUM_DRONES]; //Thread Address
-  //int *taskids[NUM_DRONES];
-  int retVal; //Holds thread return code
-  long t; //Loop Counter
+  //make our pthreads
+  pthread_t threads[NUM_DRONES]; 
+  // check for pthreade create error returns
+  int retVal; 
+  // just a regular old loop var
+  long t; 
   for(t=0;t<NUM_DRONES;t++) {
 
 
-    //drones all start at [1,1], [2,2], [3,3] ... [n,n]
+    //drones all start at [0,0], [1,1], [2,2], [3,3] ... [n,n]
     //all packages are located at arbitrary locations through the airspace
     int r1 = rand() % MAP_SIZE;
     int r2 = rand() % MAP_SIZE; 
+    // initialize drones and packages
     threadDataArr[t].thread_id = t;
     threadDataArr[t].airport_x = t;
     threadDataArr[t].airport_y = t;
     threadDataArr[t].package_x = t+r1;
     threadDataArr[t].package_y = t+r2;
-
+     
+    // check for pthreade create errs
     retVal = pthread_create(&threads[t], NULL, fly, (void *) &threadDataArr[t]);
     if (retVal) {
       printf("ERROR: return code from pthread_create() is %d\n", retVal);
       exit(-1);
     }
   }
+  // destroy mutex thread
   pthread_mutex_destroy(&mtx);
+  // exit pthreads
   pthread_exit(NULL);
 }
 
+// function to make sure drone path is clear and if not, move drones to avoid collisions
+// takes in the drone's current x,y possition, their next move to check, and a taskID which identifies the next move
 void avoid(int* currPosX, int* currPosY, char nextMove, int taskID){
     
     if(nextMove == '>'){
@@ -102,6 +125,7 @@ void avoid(int* currPosX, int* currPosY, char nextMove, int taskID){
 }
 
 // define x, y positions of obstacles and places them on the airspace
+// 
 void placeObstacles(){
     int obsx_r1 = rand() % MAP_SIZE;
     int obsy_r1 = rand() % MAP_SIZE;
@@ -148,6 +172,11 @@ void placeObstacles(){
         airspace[x][y]= 'X';
     }
 }
+//usleep (microsleep) used in this function for map reprinting isn't too fast to see
+// this function gets passed in as the third argument in pthread_create
+// initializes drones in the airspace and assigns them an ascii value
+// iterates drones through the map to their package location and then sends them home
+// avoiding collisions and reprinting the map with each iteration.
 
 void *fly(void *arg0){
   int taskID;
@@ -232,6 +261,7 @@ void *fly(void *arg0){
   pthread_exit(NULL);
 }
 
+// prints character map by printing each index in airspace
 void printMap(){
 	for(int i = 0; i < MAP_SIZE; i++){
 		for (int j = 0; j < MAP_SIZE; j++){
@@ -242,6 +272,7 @@ void printMap(){
 	return;
 }
 
+// initializes the map as just 50x50 '.'s 
 void makeMap(){
     for (int i = 0; i < MAP_SIZE; i++){
 		for (int j = 0; j < MAP_SIZE; j++){
